963a6831cbf5c0c469f86a8082eeb933340425d5
kota-6211 <kota_1126@icloud.com>
2023-06-07 11:55:41 +0000

    mypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,10 @@ class UsersController < ApplicationController
   end
 
   def mypage
+    @user = User.find_by(id: params[:user_id])
+    if @user&.id != current_user.id
+      redirect_to root_path
+    end
   end
 
   def unsubscribe
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_07_085939) do
+ActiveRecord::Schema.define(version: 2023_06_07_105356) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2023_06_07_085939) do
     t.boolean "admin", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
