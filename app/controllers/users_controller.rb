d7dea076e2385eae8fe4fa68482bb8d018900146
kota-6211 <kota_1126@icloud.com>
2023-06-08 07:17:26 +0000

    user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,34 @@
 class UsersController < ApplicationController
+  def admin_top
+    if current_user.admin != true
+      redirect_to root_path
+    end
+  end
+
   def index
+    if current_user.admin != true
+      redirect_to root_path
+    end
   end
 
   def show
+    @user = User.find_by(id: params[:id])
   end
 
   def edit
+    @user = User.find_by(id: params[:id])
+    if (current_user.admin != true) && (@user&.id != current_user.id)
+      redirect_to root_path
+    end
+  end
+
+  def update
   end
 
   def mypage
-    @user = User.find_by(id: params[:user_id])
-    if @user&.id != current_user.id
+    @user = current_user
+    @users = User.find_by(id: params[:user_id])
+    if @users&.id != current_user.id
       redirect_to root_path
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'homes#top'
   devise_for :users
-  
-  # mypageは登録情報 showはサイト内のプロフィール
+
+  get 'users/admin_top' => 'users#admin_top'
   resources :users, only: [:index, :show, :edit, :update] do
+    # mypageは登録情報 showはサイト内のプロフィール
     get :mypage
     get :unsubscribe
     patch :withdraw
