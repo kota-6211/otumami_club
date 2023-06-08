5056ad0f5dd0aa4fcd9997e9448fc8a20aaa759b
kota-6211 <kota_1126@icloud.com>
2023-06-08 15:43:07 +0000

    change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :user_state, only: [:create]
-  
+
 
 
   protected
   def user_state
     @user = User.find_by(user_name: params[:user][:user_name])
-    if @user 
+    if @user
       if @user.valid_password?(params[:user][:password]) && (@user.is_deleted == true)
         flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
         redirect_to root_path
