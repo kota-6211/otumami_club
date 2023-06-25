0bdaf06f791f168ed683f8e84ddacd3301a5050d
kota-6211 <kota_1126@icloud.com>
2023-06-25 03:42:06 +0000

    images

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,6 @@ class Ingredient < ApplicationRecord
   has_many :recipes, through: :recipe_ingredients
   has_many :recipe_indredients
   
-  validates :name, presence: true
-  validates :quantiry, presence: true
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :quantiry, presence: true, length: { maximum: 30 }
 end
--- app/models/step.rb
+++ app/models/step.rb
@@ -2,5 +2,5 @@ class Step < ApplicationRecord
   belongs_to :recipe
   
   validates :step_number, presence: true
-  validates :discription, presence: true
+  validates :discription, presence: true, length: { maximum: 30 }
 end
